4fd08cb9dd3d4a5f21dddcb588fdd11ea891a5bb
MarcioManrique <marcio.manrique@palmiq.com>
2024-11-27 11:57:15 -0500

    public static void busqueda_producto en la clase Ordenamiento_Busqueda
    Utiliza Busqueda Secuencial no ordenada
    Utiliza Arraylists
    Utiliza Insercion Directa

---
+++ src/Producto.java
@@ -0,0 +1,45 @@
+public class Producto {
+    private int codigo;
+    private String nombre;
+    private int stock;
+    private double precio;
+
+    public Producto(int codigo, String nombre, int stock, double precio) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.stock = stock;
+        this.precio = precio;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+}
